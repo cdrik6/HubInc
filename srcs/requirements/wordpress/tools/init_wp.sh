4a4efdaa70b9969d4eaaf7fcf14e602ed7db6958
Cedric Aguillon <caguillo@student.42.fr>
2025-01-20 01:05:27 +0100

    0.08

--- srcs/requirements/wordpress/tools/init_wp.sh
+++ srcs/requirements/wordpress/tools/init_wp.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # wait the database ready
-while ! wp db check --allow-root --path=/var/www/html/; do
+while ! wp db check --allow-root --path=/var/www/wordpress/; do
     echo "Waiting for Database to be ready..."
     sleep 1
 done
 
 if ! -e /var/www/wordpress/wp-config.php; then
     
-	wp config create --allow-root --dbname=$SQL_DB --dbuser=$SQL_USER --dbpass=$SQL_USER_PWD \
+	wp config create --allow-root --dbname=$SQL_DB_NAME --dbuser=$SQL_USER --dbpass=$SQL_USER_PWD \
     				 --dbhost=mariadb:3306 --path='/var/www/wordpress'
 	
-	wp core install --url=$DOMAIN_NAME --title=$TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PWD \
+	wp core install --url=$DOMAIN_NAME --title=$TITLE --admin_user=$ADMIN --admin_password=$ADMIN_PWD \
 					--admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
 	
-	wp user create --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PWD \
+	wp user create --allow-root --role=author $USER $USER_MAIL --user_pass=$USER_PWD \
 				   --path='/var/www/wordpress' >> /log.txt
 fi
 
@@ -24,4 +24,4 @@ if ! -d /run/php; then
 fi
 
 # launch php-fpm
-/usr/sbin/php-fpm8.4 -F
+/usr/sbin/php-fpm7.4 -F
