0e2ce755346b7d98245bf0abd1e90583930a2fb5
cdrik6 <cdrik6@free.fr>
2025-02-01 02:15:00 +0100

    0.16

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -38,33 +38,45 @@ done
 # chmod 755 /var/run/mysqld/mysqld.sock
 # chown -R mysql:mysql /var/run/mysqld/mysqld.sock 
 
+echo "1:"
 # create the DB SQL_DB_NAME (define in .env)
 mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DB_NAME};"
-echo "1"
 
+echo "2:"
 # create the user SQL_USER of the DB (define in .env)
 # 'localhost'
 mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_USER_PWD}';"
-echo "2"
 
+echo "3:"
 # give whole rigths the user SQL_USER of the DB (define in .env)
 # % -> allows the user to connect from any host
 mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DB_NAME}.* TO '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_USER_PWD}';"
-echo "3"
 
+echo "4:"
 # change/set the password for the MySQL root user (only when accessed from the local machine) --> more secure
 mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-echo "4"
 
+echo "5: flush"
 # update new parameters
 mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
-echo "5"
 
+echo "5b: socket?"
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "SHOW VARIABLES LIKE 'socket';"
+
+echo "6: shutdown"
 # first switch off mysql
-mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" --socket='var/run/mysqld/mysqld.sock' shutdown
-echo "6"
+mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" --user=root --socket='/run/mysqld/mysqld.sock' shutdown
 
+echo "6b: socket?"
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "SHOW VARIABLES LIKE 'socket';"
+
+echo "7: mysqld_safe"
 # then restart mysql in order it runs with new parameters
-mysqld_safe --user=mysql --port=3306 --bind-address=0.0.0.0 --socket='var/run/mysqld/mysqld.sock' --datadir='/var/lib/mysql'
+mysqld_safe --user=mysql --port=3306 --bind-address=0.0.0.0 --socket='/run/mysqld/mysqld.sock' --datadir='/var/lib/mysql'
+#mysqld_safe
+# systemctl start mariadb
+
+echo "7b: socket?"
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "SHOW VARIABLES LIKE 'socket';"
 
 echo "Init MariaDB done!"
