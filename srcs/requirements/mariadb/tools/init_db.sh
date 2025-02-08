507ccfdea4aafb67b4c98c1b8b916ccb8047a13a
cdrik6 <cdrik6@free.fr>
2025-02-08 21:32:06 +0100

    0.23

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 service mariadb start
 
 # Wait for MariaDB to be ready
@@ -8,56 +7,52 @@ until mysqladmin ping -h localhost --silent; do
     sleep 1
 done
 
-# Check correct/regular names and pwds for mysql 
-# 
-# if [ -z "${SQL_DB_NAME}" ]; then
-#   echo "Error: SQL_DB_NAME is not set."
-#   exit 1
-# fi
-# 
-# if [ -z "${SQL_USER}" ]; then
-#   echo "Error: SQL_USER is not set."
-#   exit 1
-# fi
-# 
-# if [ -z "${SQL_USER_PWD}" ]; then
-#   echo "Error: SQL_USER_PWD is not set."
-#   exit 1
-# fi
-# 
-# if [ -z "${MYSQL_ROOT_PASSWORD}" ]; then
-#   echo "Error: MYSQL_ROOT_PASSWORD is not set."
-#   exit 1
-# fi
+# Configaration file my.cnf
+echo
+cat etc/mysql/my.cnf
 
-echo "1:"
-# create the DB SQL_DB_NAME (define in .env)
-mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DB_NAME};"
+# Check correct/regular names and pwds for mysql
+if [ -z "${DB_NAME}" ]; then
+  echo "Error: DB_NAME is not set."
+  exit 1
+fi
+if [ -z "${MYSQL_USER}" ]; then
+  echo "Error: SQL_USER is not set."
+  exit 1
+fi
+if [ -z "${MYSQL_USER_PWD}" ]; then
+  echo "Error: SQL_USER_PWD is not set."
+  exit 1
+fi
+echo "Names and Passwords: OK"
 
-echo "2:"
-# create the user SQL_USER of the DB (define in .env)
-mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_USER_PWD}';"
-
-echo "3:"
-# give whole rigths the user SQL_USER of the DB (define in .env)
-# % -> allows the user to connect from any host
-# The GRANT OPTION privilege allows a user to pass on any privileges she has to other users
-mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DB_NAME}.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_USER_PWD}';"
-
-echo "4: socket?"
+# socket ?
 mysql -e "SHOW VARIABLES LIKE 'socket';"
 
-echo "5: flush"
-# update new parameters
-mysql -e "FLUSH PRIVILEGES;"
-
-echo "6: stop"
-# first switch off
-service mariadb stop
-
-echo "7: mysqld_safe"
-# then restart mysql in order it runs with new parameters
-# mysqld_safe --user=mysql --port=3306 --bind-address=0.0.0.0 --socket='/run/mysqld/mysqld.sock' --datadir='/var/lib/mysql'
-mysqld_safe
-
+# create the DB DB_NAME (define in .env)
+mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
 mysql -e "SHOW DATABASES;"
+
+# create the user MYSQL_USER of the DB (define in .env)
+# % -> allows the user to connect from any host
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_USER_PWD}';"
+mysql -e "SELECT user, host FROM mysql.user;"
+
+# give whole rigths the user MYSQL_USER of the DB (define in .env)
+mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${MYSQL_USER}'@'%';"
+
+# MySQL updates privileges automatically. 
+# Flush privileges ensures that changes take effect immediately.
+mysql -e "FLUSH PRIVILEGES;"
+mysql -e "SELECT user, host, authentication_string, plugin FROM mysql.user;"
+mysql -e "SHOW GRANTS FOR '${MYSQL_USER}'@'%';"
+
+# status
+mysqladmin status
+
+# & = running in background
+mysqld_safe &
+
+# note:
+# Service mariadb stop or mysqladmin shutdown before restart is unusefull
+# In worst case, flush privileges does the job
