6694127b7b02c25619166cca95e3f35378a1fafe
Cedric Aguillon <caguillo@student.42.fr>
2025-02-12 23:06:39 +0100

    0.33

--- srcs/requirements/wordpress/tools/init_wp.sh
+++ srcs/requirements/wordpress/tools/init_wp.sh
@@ -1,43 +1,35 @@
 #!/bin/bash
 
+# ensure mariadb is running before continuing
+sleep 10
+
 # # wait the database ready -- pb : check wp-config.php
 # while ! wp db check --allow-root --path=/var/www/wordpress/; do
 #     echo "Waiting for Database to be ready..."
 #     sleep 1
 # done
 
-# ensure mariadb is running before continuing
-sleep 10
-
-# if [ -e /var/www/wordpress/wp-config.php ]; then
-# 	echo "wp-config exits"
-# else
-# 	echo "wp-config don't"	
-# fi
+cd /var/www/wordpress
 
 # if wp-config.php file does not exist
-if [ ! -e /var/www/wordpress/wp-config.php ]; then
-	echo "00"
-	cd /var/www/wordpress
-	echo "0"
+if [ -f /wp-config.php ]; then
+	echo "Wordpress already installed"
+else 
 	wp config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_USER_PWD" \
     				 --dbhost=mariadb:3306 --allow-root --path='/var/www/wordpress'	
-	echo "1"				 
 	wp core install --url="$DOMAIN_NAME" --title="$TITLE" --admin_user="$ADMIN" --admin_password="$ADMIN_PWD" \
-					--admin_email="$ADMIN_EMAIL" --allow-root --path='/var/www/wordpress'	
-	echo "2"				
+					--admin_email="$ADMIN_EMAIL" --allow-root --path='/var/www/wordpress'
 	wp user create  "$USER" "$USER_EMAIL" --user_pass="$USER_PWD" --role=author \
-				   	--allow-root --path='/var/www/wordpress'
-	echo "3"			   
+				   	--allow-root --path='/var/www/wordpress'	
 	# ensure any cached data is cleared to avoid conflicts
-	wp cache flush --allow-root --path='/var/www/wordpress'
-	echo "4"
+	wp cache flush --allow-root --path='/var/www/wordpress'	
 fi
+# -e = exists ? (file or directory)
+# -f = file exists ?
+# -d = directory exists ?
 
-if [ -e /var/www/wordpress/wp-config.php ]; then
-	echo "2 - wp-config exits"
-else
-	echo "2 - wp-config don't"	
+if [ ! -e /var/www/wordpress/wp-config.php ]; then
+	echo "wp-config.php is missing"	
 fi
 
 # if /run/php folder does not exist, create it
@@ -45,9 +37,5 @@ if [ ! -d /run/php ]; then
     mkdir -p /run/php
 fi
 
-echo "5"
-
 # launch php-fpm
 /usr/sbin/php-fpm7.4 -F
-
-echo "6"
